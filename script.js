8a49980f8dd4af51b912eadd2135d8626ba23b26
Tewxzy <klebersomoliveira90@gmail.com>
2024-08-27 11:24:59 -0300

    broken-lamp

--- script.js
+++ script.js
@@ -2,22 +2,28 @@ let ativo = document.getElementById('img')
 let system = 'off'
 
 function on(){
+  
   ativo.src =  'img/lamp-on.jpg'
+
   system = 'on'
-  console.log(system)
 }
 
 function off(){
+
   ativo.src =  'img/lamp-off.jpg'
+
   system = 'off'
-  console.log(system)
 }
 
 ativo.addEventListener("mouseenter",function() {
    
     if(system === 'off'){
+
       ativo.src =  'img/lamp-on.jpg'
+
       system = 'on'
+    }else{
+      ativo.src =  system
     }
 
 })
@@ -25,8 +31,27 @@ ativo.addEventListener("mouseenter",function() {
 ativo.addEventListener("mouseleave",function() {
     
     if(system === 'on'){
+      
       ativo.src =  'img/lamp-off.jpg'
+      
       system = 'off'
+    }else{
+      ativo.src =  system
     }
 
+})
+
+let clicks = 0
+ativo.addEventListener("click",function() {  
+  
+  clicks++
+
+  if(clicks == 2){
+
+    ativo.src =  "img/broken-lamp.jpg"
+
+    system = "img/broken-lamp.jpg"
+
+    clicks = 0
+  }
 })
