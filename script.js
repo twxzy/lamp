7647d37a339183cd467582f90bdf27e6f2128d38
Tewxzy <klebersomoliveira90@gmail.com>
2024-08-27 10:35:53 -0300

    mouse-over

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 let ativo = document.getElementById('img')
+let system = 'off'
 
 function on(){
   ativo.src =  'img/lamp-on.jpg'
+  system = 'on'
+  console.log(system)
 }
 
 function off(){
   ativo.src =  'img/lamp-off.jpg'
+  system = 'off'
+  console.log(system)
 }
+
+ativo.addEventListener("mouseenter",function() {
+   
+    if(system === 'off'){
+      ativo.src =  'img/lamp-on.jpg'
+      system = 'on'
+    }
+
+})
+
+ativo.addEventListener("mouseleave",function() {
+    
+    if(system === 'on'){
+      ativo.src =  'img/lamp-off.jpg'
+      system = 'off'
+    }
+
+})
